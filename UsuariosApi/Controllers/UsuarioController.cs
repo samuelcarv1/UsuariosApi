ff550d11ccbfdc0f6d9555306d1860a1a2916878
Samuel Carvalho <samuelcarvalho2000@outlook.com>
2025-01-08 13:59:06 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private CadastroService _cadastroService;
+
+        public UsuarioController(CadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+        {
+
+            await _cadastroService.Cadastra(dto);
+            return Ok("Usuário Cadastrado!");
+
+        }
+    }
+}
---
+++ UsuariosApi/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("UsuarioConnection");
+
+builder.Services.AddDbContext<UsuarioDbContext>
+    (opts =>
+    {
+        opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    });
+//Identity, eu quero adicionar o conceito de identidade para esse usuário, e o papel desse usuário (IdentityRole) dentro do nosso sistema também será gerenciado por você
+builder.Services.AddIdentity<Usuario, IdentityRole>()
+                .AddEntityFrameworkStores<UsuarioDbContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<CadastroService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
